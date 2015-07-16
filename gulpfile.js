6ce340dc77c4b41b9c64a079cf41c7139739d2c0
Stephen <leard@stlzoo.org>
2015-07-16 15:24:38 -0500

    more gulpfile.js updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,92 @@
-var gulp = require('gulp'),
-		gutil = require('gulp-util'),
-		browserify = require('gulp-browserify'),
-		compass = require('gulp-compass'),
-		connect = require('gulp-connect'),
-		gulpif = require('gulp-if'),
-		uglify = require('gulp-uglify'),
-		minifyHTML = require('gulp-minify-html'),
-		concat = require('gul-concat'),
-		path = require('path');
+//modified from gulpfil.js in //https://github.com/planetoftheweb/responsive,
+//the repo for the awesome lynda.com course 'Building a Responsive Single-Page Design,'
+//by Ray Villalobos, which is awesome.
 
-var env, jsSources, sassSources, htmlSources, outputDir, sassStyle;
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    concat = require('gulp-concat'),
+    path = require('path');
+
+var env,
+    jsSources,
+    sassSources,
+    htmlSources,
+    outputDir,
+    sassStyle;
 
 env = 'development';
 
-if (env === 'development') {
-	outputDir = 'pressd/devo';
-	sassStyle = 'expanded';
+if (env==='development') {
+  outputDir = 'pressd/devo/';
+  sassStyle = 'expanded';
 } else {
-	outputDir = 'pressd/pro';
-	sassStyle = 'compressed';
+  outputDir = 'pressd/pro/';
+  sassStyle = 'compressed';
 }
 
-jsSourcs = [
-	'compd/scripts/jqloader.js',
-	'compd/scripts/TweenMax.min.js',
-	'compd/scripts/jquery.scrollmagic.min.js',
-	'compd/scripts/script.js'
+jsSources = [
+  'compd/scripts/jqloader.js',
+  'compd/scripts/TweenMax.min.js',
+  'compd/scripts/jquery.scrollmagic.min.js',
+  'compd/scripts/script.js'
 ];
-
 sassSources = ['compd/sass/style.scss'];
-
 htmlSources = [outputDir + '*.html'];
 
-gulp.task('js',function(){
-	gulp.src(jsSources)
-			.pipe(concat('script.js'))
-			.pipe(browserify())
-			.on('error',gutil.log)
-			.pipe(gulpif(env === 'production',uglify()))
-			.pipe(gulp.dest(outputDir + 'js'))
-			.pipe(connect.reload())
+gulp.task('js', function() {
+  'use strict';
+
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .on('error', gutil.log)
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload());
 });
 
-gulp.task('compass',function(){
-	gulp.src(sassSources)
-			.pipe(compass({
-			sass: 'compd/sass',
-			css: outputDir + 'css',
-			image: outputDir + 'images',
-			style: sassStyle,
-			require: ['susy','breakpoint']
-			})
-			.on('error',gutil.log))
-			// .pipe(gulp.dest(outputDir + 'css'))
-			.pipe(connect.reload())
+gulp.task('compass', function() {
+  'use strict';
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'compd/sass',
+      css: outputDir + 'css',
+      image: outputDir + 'images',
+      style: sassStyle,
+      require: ['susy', 'breakpoint']
+    })
+    .on('error', gutil.log))
+//    .pipe(gulp.dest( outputDir + 'css'))
+    .pipe(connect.reload());
 });
 
-gulp.task('watch',function(){
-	gulp.watch(jsSources, ['js']);
-	gulp.watch(['compd/sass/*.scss','compd/sass/*/*.scss'],['compass']);
-	gulp.watch('pressd/devo/*.html',['html']);
+gulp.task('watch', function() {
+  'use strict';
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(['compd/sass/*.scss', 'compd/sass/*/*.scss'], ['compass']);
+  gulp.watch('pressd/devo/*.html', ['html']);
 });
 
-gulp.task('connect',function(){
-	connect.server({
-		root: outputDir,
-		livereload: true
-	});
+gulp.task('connect', function() {
+  'use strict';
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
 });
 
-gulp.task('html',function(){
-	gulp.src('pressd/devo/*.html')
-			.pipe(gulpif(env === 'production', minifyHTML()))
-			.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
-			.pipe(connect.reload)
+gulp.task('html', function() {
+  'use strict';
+  gulp.src('pressd/devo/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload());
 });
 
-gulp.task();
-
-
-
-
+gulp.task('default', ['watch', 'html', 'js', 'compass', 'connect']);
