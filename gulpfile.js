0bdba3de966fc14a984a9fc10ffa62741160ecde
Stephen <leard@stlzoo.org>
2015-07-16 15:05:55 -0500

    update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -50,14 +50,31 @@ gulp.task('compass',function(){
 			image: outputDir + 'images',
 			style: sassStyle,
 			require: ['susy','breakpoint']
-	}))
+			})
+			.on('error',gutil.log))
+			// .pipe(gulp.dest(outputDir + 'css'))
+			.pipe(connect.reload())
 });
 
-gulp.task();
+gulp.task('watch',function(){
+	gulp.watch(jsSources, ['js']);
+	gulp.watch(['compd/sass/*.scss','compd/sass/*/*.scss'],['compass']);
+	gulp.watch('pressd/devo/*.html',['html']);
+});
 
-gulp.task();
+gulp.task('connect',function(){
+	connect.server({
+		root: outputDir,
+		livereload: true
+	});
+});
 
-gulp.task();
+gulp.task('html',function(){
+	gulp.src(pressd/devo.*.html)
+			.pipe(gulpif(env === 'production', minifyHTML()))
+			.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+			.pipe(connect.reload)
+});
 
 gulp.task();
 
